8e56e4542da9f4d9d90933ecb223a5056147bfcc
carlos_manjarres <manjarresc63@gmail.com>
2024-05-27 17:20:57 -0500

    ya realice 3 tablas

--- OneDrive - TCC/Documentos/fulsport_tcc-main/src/main/java/com/fullsport/persistence/entity/CategoriaProducto.java
+++ OneDrive - TCC/Documentos/fulsport_tcc-main/src/main/java/com/fullsport/persistence/entity/CategoriaProducto.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class CategoriaProducto {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "categoria_id")
     private Integer categoriaId;
 
